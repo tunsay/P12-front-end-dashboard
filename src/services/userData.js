655be0fa1df204dccae5626571ad5d70c3226ddd
tunsay <tuna1997@hotmail.fr>
2023-05-04 17:09:46 +0200

    Complete User Data

--- src/services/userData.js
+++ src/services/userData.js
@@ -16,3 +16,52 @@ export async function getUserData(id) {
     console.log(error)
   }
 }
+
+/**
+ * Get user activity data from API
+ * @async
+ * @param {string} id - user id
+ * @returns {Promise<object>} A promise that contains an object with the user's activity data when resolved
+ */
+export async function getUserActivity(id) {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`http://localhost:3000/user/${id}/activity`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/**
+ * Get user performance data from API
+ * @param {string} id - user id
+ * @returns {Promise<object>} A promise that contains an object with the user's average sessions data when resolved
+ */
+export async function getUserPerformance(id) {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`http://localhost:3000/user/${id}/performance`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/**
+ * Get user average sessions data from API
+ * @param {string} id - user id
+ * @returns {Promise<object>} A promise that contains an object with the user's performance data when resolved
+ */
+export async function getUserAverageSessions(id) {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`http://localhost:3000/user/${id}/average-sessions`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
